09813f16c73ae28e572bc07731f85d51b43fdc82
Lucas Barros <lucasbarrosez@gmail.com>
2021-01-19 12:32:29 -0300

    feat: method find by id created

--- src/main/java/com/algaworks/osworks/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/controller/ClienteController.java
@@ -1,13 +1,11 @@
 package com.algaworks.osworks.controller;
 
-import java.util.Arrays;
 import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
@@ -15,13 +13,20 @@ import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
 public class ClienteController {
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{clienteid}")
+	public Cliente buscar(@PathVariable Long clienteId) {
+		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+		
+		return cliente.orElse(null);
 	}
 
 }
