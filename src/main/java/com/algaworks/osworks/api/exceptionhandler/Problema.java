4c568f0d3e733d3342aba9613d24ffe42778c5de
Lucas Barros <lucasbarrosez@gmail.com>
2021-01-19 16:22:52 -0300

    feat: exception inserted

---
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/Problema.java
@@ -0,0 +1,45 @@
+package com.algaworks.osworks.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Problema {
+
+	private Integer status;
+	private LocalDateTime dataHora;
+	private String titulo;
+	private List<Campo> campos;
+
+	public List<Campo> getCampos() {
+		return campos;
+	}
+
+	public void setCampos(List<Campo> campos) {
+		this.campos = campos;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getDataHora() {
+		return dataHora;
+	}
+
+	public void setDataHora(LocalDateTime dataHora) {
+		this.dataHora = dataHora;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+}
