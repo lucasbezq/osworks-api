0ff24b09899f9f797b08f47b944e3b9ddfa6ef7e
Lucas Barros <lucasbarrosez@gmail.com>
2021-01-19 14:16:48 -0300

    feat: method create insert

--- src/main/java/com/algaworks/osworks/controller/ClienteController.java
+++ src/main/java/com/algaworks/osworks/controller/ClienteController.java
@@ -4,29 +4,45 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.osworks.domain.model.Cliente;
 import com.algaworks.osworks.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
 	@Autowired
 	private ClienteRepository clienteRepository;
 
-	@GetMapping("/clientes")
+	@GetMapping
 	public List<Cliente> listar() {
 		return clienteRepository.findAll();
 	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
 	
-	@GetMapping("/clientes/{clienteid}")
-	public Cliente buscar(@PathVariable Long clienteId) {
-		Optional<Cliente> cliente = clienteRepository.findById(clienteId);
-		
-		return cliente.orElse(null);
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente adicionar(@RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
 	}
 
 }
