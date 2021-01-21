62e8bc849aab4d6f3a5677601277680d5a9e65a6
Lucas Barros <lucasbarrosez@gmail.com>
2021-01-21 10:23:55 -0300

    feat: delegating responsabilities

--- src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/osworks/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,9 +3,13 @@ package com.algaworks.osworks.api.exceptionhandler;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -15,19 +19,21 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+	@Autowired
+	private MessageSource messageSource;
+
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
-		
+
 		var campos = new ArrayList<Campo>();
 		for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-			String nome = error.getObjectName();
-			String mensagem = error.getDefaultMessage();
-			
+			String nome = ((FieldError) error).getField();
+			String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
+
 			campos.add(new Campo(nome, mensagem));
 		}
-		
+
 		var problema = new Problema();
 		problema.setStatus(400);
 		problema.setTitulo("Um ou mais campos estão inválidos. Faça o o preenchimento" + "correto e tente novamente");
---
+++ src/main/java/com/algaworks/osworks/domain/service/CadastroClienteService.java
@@ -0,0 +1,18 @@
+package com.algaworks.osworks.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.algaworks.osworks.domain.model.Cliente;
+import com.algaworks.osworks.domain.repository.ClienteRepository;
+
+public class CadastroClienteService {
+	
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	public Cliente salvar(Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	
+}
