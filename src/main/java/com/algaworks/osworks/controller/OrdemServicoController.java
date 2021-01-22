a5c77dd0ba8d3f6a69fc64e92404524b9fe8e3fb
Lucas Barros <lucasbarrosez@gmail.com>
2021-01-22 11:18:51 -0300

    feat: method find by id created

--- src/main/java/com/algaworks/osworks/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/controller/OrdemServicoController.java
@@ -1,12 +1,15 @@
 package com.algaworks.osworks.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,6 +39,16 @@ public class OrdemServicoController {
 	@GetMapping
 	public List<OrdemServico> listar() {
 		return ordemServicoRepository.findAll();
+	}
+
+	@GetMapping("/{ordemServicoId}")
+	public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId) {
+		Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+
+		if (ordemServico.isPresent()) {
+			return ResponseEntity.ok(ordemServico.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 
 }
